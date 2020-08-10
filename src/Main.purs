f6d6a552ea1f0b23831a6ec24df8b88b2ebe89fd
Andreas Kollegger <akollegger@gmail.com>
2020-08-10 12:03:31 +0200

    added bracket-only toPath rendering; added toXML rendering

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,50 @@ module Main where
 
 import Prelude
 
+import Data.SimplePath (Path(..), edge, node, toGram, toPath, toXML)
 import Effect (Effect)
 import Effect.Console (log)
 
+x :: Path
+x  = EmptyPath
+
+n1 :: Path
+n1 = node
+
+n2 :: Path
+n2 = node
+
+n3 :: Path
+n3 = node
+
+n4 :: Path
+n4 = node
+
+e1 :: Path
+e1 = edge n1 n2
+
+e2 :: Path
+e2 = edge n3 n4
+
+p1 :: Path
+p1 = Path e1 e2
+
 main :: Effect Unit
 main = do
-  log "🍝"
+  log $ "empty: " <> show x 
+  log $ "\t" <> toPath x
+  log $ "\t" <> toGram x
+  log $ "\t" <> toXML x
+  log $ "node:  " <> show n1
+  log $ "\t" <> toPath n1 
+  log $ "\t" <> toGram n1
+  log $ "\t" <> toXML n1
+  log $ "edge:  " <> show e1 
+  log $ "\t" <> toPath e1
+  log $ "\t" <> toGram e1
+  log $ "\t" <> toXML e1
+  log $ "path:  " <> show p1 
+  log $ "\t" <> toPath p1
+  log $ "\t" <> toGram p1
+  log $ "\t" <> toXML p1
+  
